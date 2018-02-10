daa640886501a2b44a3d0a54f6950f64dc65a95b
Lav39 <lavkushkumar7654@gmail.com>
2018-02-10 23:44:07 +0530

    initial

---
+++ script/app.js
@@ -0,0 +1,11 @@
+(function(){
+    'use strict';
+    function AppController(timeout){
+        var app = this;
+    
+        
+    }
+    var myApp = angular.module('myApp',[]);
+    myApp.controller("AppController",["$timeout",AppController])
+
+})();
